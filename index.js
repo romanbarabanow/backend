4f0b39c4647c0884bd63b5ecc86c8c2d3f1dd9f7
Roman <ketzalec@gmail.com>
2023-06-14 17:17:56 +0300

    SERVER

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 
+mongoose
+  .connect("mongodb://localhost:27017/yachts", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err))
+
 const yachtsRouter = require("./routes/yachts.route")
 const userRouter = require("./routes/user.route")
 const townRouter = require("./routes/town.route")
@@ -9,20 +16,12 @@ const townRouter = require("./routes/town.route")
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/api", yachtsRouter)
 app.use("/api", userRouter)
 app.use("/api", townRouter)
 
-mongoose
-  .connect("mongodb://45.12.75.52:27017/yachts", {
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err))
-
-app.use(cors())
-
-app.listen(5050, () => {
+app.listen(4000, () => {
   console.log("Server is running")
 })
