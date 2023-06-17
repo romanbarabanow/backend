6955b754097167259a8365972366630869898f3e
Roman <ketzalec@gmail.com>
2023-06-17 12:23:55 +0300

    serversFinally

--- routes/yachts.route.js
+++ routes/yachts.route.js
@@ -7,5 +7,6 @@ router.post("/yachts", YachtsControllers.createYachts)
 router.get("/yachts", YachtsControllers.getAllYachts)
 router.delete("/yachts", YachtsControllers.deleteYachts)
 router.get("/yachts/town", YachtsControllers.getYachtsbyTown)
+router.get("/yachts/id", YachtsControllers.getYachtsbyId)
 
 module.exports = router
