82dcc903c9f3fecd046084a7b9e33401014acc55
Roman <ketzalec@gmail.com>
2023-07-09 20:12:19 +0300

    finall

--- backend/controllers/MailController.js
+++ backend/controllers/MailController.js
@@ -2,23 +2,23 @@ const TelegramBot = require("node-telegram-bot-api")
 
 const token = "5401350979:AAHRBKT7aLbW-peUg_sGN1c6K1_R9vpaZ4s"
 
-// const bot = new TelegramBot(token, { polling: true })
+const bot = new TelegramBot(token, { polling: true })
 
 const sendMail = async (req, res) => {
   const { phone, name, yacht } = req.query
 
-  // bot
-  //   .sendMessage(
-  //     765149596,
-  //     `
-  //   У вас новое письмо от ${name}
-  // его номер телефона ${phone}
-  // Он хочет заказать яхту ${yacht}
-  // `
-  //   )
-  //   .then(() => {
-  //     res.json({ status: "ok" }).status(200)
-  //   })
+  bot
+    .sendMessage(
+      765149596,
+      `
+    У вас новое письмо от ${name}
+  его номер телефона ${phone}
+  Он хочет заказать яхту ${yacht}
+  `
+    )
+    .then(() => {
+      res.json({ status: "ok" }).status(200)
+    })
 }
 
 module.exports = {
