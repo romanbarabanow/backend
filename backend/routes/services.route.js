3cfca6c275ff4f1ab3c3194eee4fc47667014963
Roman <ketzalec@gmail.com>
2023-07-10 18:01:15 +0300

    change

--- backend/routes/services.route.js
+++ backend/routes/services.route.js
@@ -8,7 +8,7 @@ const router = Router()
 
 router.get("/services", ServicesController.getService)
 router.post("/services", middleware, ServicesController.changeService)
-router.get("/services/catering", ServicesController.changeCatering)
-router.post("/services/catering", ServicesController.getCatering)
+router.get("/services/catering", ServicesController.getCatering)
+router.post("/services/catering", ServicesController.changeCatering)
 
 module.exports = router
