9d0ef3b42fd684ae02f7fe7b05cdf0b9caa99303
Roman <ketzalec@gmail.com>
2023-07-10 13:22:42 +0300

    catering page

--- backend/routes/services.route.js
+++ backend/routes/services.route.js
@@ -8,5 +8,7 @@ const router = Router()
 
 router.get("/services", ServicesController.getService)
 router.post("/services", middleware, ServicesController.changeService)
+router.get("/services/catering", ServicesController.changeCatering)
+router.post("/services/catering", ServicesController.getCatering)
 
 module.exports = router
